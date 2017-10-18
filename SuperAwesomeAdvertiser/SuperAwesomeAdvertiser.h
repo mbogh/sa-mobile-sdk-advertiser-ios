6788bb59ec3a4337f453b696e477cb957603f4a0
Tudor-Dan Balas <tudor@halcyonmobile.com>
2017-10-18 09:43:18 +0300

    1.0.10 - Added Carthage support

---
+++ SuperAwesomeAdvertiser/SuperAwesomeAdvertiser.h
@@ -0,0 +1,23 @@
+//
+//  SuperAwesomeAdvertiser.h
+//  SuperAwesomeAdvertiser
+//
+//  Created by Tudor-Dan Balas on 10/18/17.
+//  Copyright © 2017 Tudor-Dan Balas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SuperAwesomeProject/SAVerifyInstall.h>
+#import <SuperAwesomeProject/SAOnce.h>
+#import <SuperAwesomeProject/SAInstall.h>
+#import <SuperAwesomeProject/SAAdvUtils.h>
+
+//! Project version number for SuperAwesomeAdvertiser.
+FOUNDATION_EXPORT double SuperAwesomeAdvertiserVersionNumber;
+
+//! Project version string for SuperAwesomeAdvertiser.
+FOUNDATION_EXPORT const unsigned char SuperAwesomeAdvertiserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperAwesomeAdvertiser/PublicHeader.h>
+
+
